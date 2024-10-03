0a87401ad083e96e0a9574354fec9a33749aee8c
kretov <kretov@samsonpost.ru>
2024-10-03 15:45:48 +0300

    Добавление chakra ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import { useState } from "react";
-import "./App.css";
+import { Button, ButtonGroup } from "@chakra-ui/react";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Button colorScheme="blue">Button</Button>
+    </div>
+  );
 }
 
 export default App;
