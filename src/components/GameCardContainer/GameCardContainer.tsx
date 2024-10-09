fe15c09b0015d2ee2e0c72cf50f1ec24f73a0f05
kretov <kretov@samsonpost.ru>
2024-10-09 16:48:41 +0300

    No Image для игр без картинок

--- src/components/GameCardContainer/GameCardContainer.tsx
+++ src/components/GameCardContainer/GameCardContainer.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const GameCardContainer = ({ children }: Props) => {
   return (
-    <Box borderEndRadius={10} overflow="hidden">
+    <Box borderRadius={10} overflow="hidden">
       {children}
     </Box>
   );
