1eea2f086d865d8b7578d61b2fa1b35a2717e325
kretov <kretov@samsonpost.ru>
2024-10-09 15:14:14 +0300

    Улучшение списка жанров

--- src/components/GameCardContainer/GameCardContainer.tsx
+++ src/components/GameCardContainer/GameCardContainer.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const GameCardContainer = ({ children }: Props) => {
   return (
-    <Box width="300px" borderEndRadius={10} overflow="hidden">
+    <Box borderEndRadius={10} overflow="hidden">
       {children}
     </Box>
   );
