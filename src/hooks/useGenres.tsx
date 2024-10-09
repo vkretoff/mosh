aff20cc344c7c206265b9ccef66ade48554cea89
kretov <kretov@samsonpost.ru>
2024-10-09 15:00:07 +0300

    Универсальный хук для получения данных

--- src/hooks/useGenres.tsx
+++ src/hooks/useGenres.tsx
@@ -1,41 +1,10 @@
-import { useEffect, useState } from "react";
-import ApiClient from "../services/ApiClient";
-import { CanceledError } from "axios";
+import useData from "./useData";
 
 export interface Genre {
   id: number;
   name: string;
 }
 
-export interface FetchGenresResponse {
-  count: number;
-  results: Genre[];
-}
-
-const useGenres = () => {
-  const [genres, setGenres] = useState<Genre[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const controller = new AbortController();
-
-    setLoading(true);
-
-    ApiClient.get<FetchGenresResponse>("/genres", { signal: controller.signal })
-      .then((res) => {
-        setGenres(res.data.results);
-        setLoading(false);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setLoading(false);
-      });
-    return () => controller.abort();
-  }, []);
-
-  return { genres, error, isLoading };
-};
+const useGenres = () => useData<Genre>("/genres");
 
 export default useGenres;
